9b4244f1ab2a9a3aa024c1b9c8697159de5c65f6
Rizki <riskiputradandi@gmail.com>
2026-06-27 16:22:30 +0700

    feat: initialize monorepo structure with NestJS backend and Golang worker projects

---
+++ worker/cmd/worker/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	fmt.Println("InvoiceGo Golang Worker starting...")
+	log.Println("Ready to process receipt OCR tasks.")
+}
---
+++ worker/go.mod
@@ -0,0 +1,3 @@
+module invoicego/worker
+
+go 1.22.0
